482218deacbdb4884077493bba144ddd284b45bf
Henrique Lima <henriqueal@algartelecom.com.br>
2017-02-22 16:29:18 -0300

    alterações sugeridas pelo Ricardo e os respectivos resultados

--- ackermann.c
+++ ackermann.c
@@ -1,7 +1,7 @@
-int Ack(int M, int N) {
+int ackermann(int M, int N) {
     if (M == 0) return( N + 1 );
-    if (N == 0) return( Ack(M - 1, 1) );
-    return( Ack(M - 1, Ack(M, (N - 1))) );
+    if (N == 0) return( ackermann(M - 1, 1) );
+    return( ackermann(M - 1, ackermann(M, (N - 1))) );
 }
 
 
